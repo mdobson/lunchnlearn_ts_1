ce55af7cf5e4844818d2be904ff089e5c1ca2ac7
Matt Dobson <mdobson4@gmail.com>
2022-04-03 13:17:32 -0400

    fix: refactor tip 6 for terseness

--- src/tip_6.ts
+++ src/tip_6.ts
@@ -1,37 +1,23 @@
 //Typing function expressions
 
-const add = (x: number, y: number): number => {
-  return x + y;
-};
+const add = (x: number, y: number): number => x + y;
 
-const sub = (x: number, y: number): number => {
-  return x - y;
-};
+const sub = (x: number, y: number): number => x - y;
 
-const mul = (x: number, y: number): number => {
-  return x * y;
-};
+const mul = (x: number, y: number): number => x * y;
 
-const div = (x: number, y: number): number => {
-  return x / y;
-};
+const div = (x: number, y: number): number => x / y;
 
 //Adding a type to functions with a common signature can clean up repeated code
 
 type NumericOperation = (x: number, y: number) => number;
 
-const typedAdd: NumericOperation = (x, y) => {
-  return x + y;
-};
+const typedAdd: NumericOperation = (x, y) => x + y;
 
-const typedSub: NumericOperation = (x, y) => {
-  return x - y;
-};
+const typedSub: NumericOperation = (x, y) => x - y;
 
-const typedMul: NumericOperation = (x, y) => {
-  return x * y;
-};
+const typedMul: NumericOperation = (x, y) => x * y;
 
-const typedDiv: NumericOperation = (x, y) => {
-  return x / y;
-};
+const typedDiv: NumericOperation = (x, y) => x / y;
+
+console.log(`Add ${add(2, 3)} and typed add ${typedAdd(2, 3)}!`);
